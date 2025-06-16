526d3655dc48b4a1a52100b899f666a5a0abbfc8
joão.sousa <joao.sousa0164@gmail.com>
2025-06-16 18:51:33 -0300

    Update cards.php

--- eco scaling/cards.php
+++ eco scaling/cards.php
@@ -16,7 +16,7 @@
   <a class="logo">EcoScaling</a>
   <div class="nav-menu-right">
     <ul class="nav-list">
-      <li><a href="inicio.php">Início</a></li>
+      <li><a href="index.php">Início</a></li>
       <li><a href="ecogame.php">Ecogame</a></li>
       <li><a href="sobre.php">Sobre</a></li>
     </ul>
